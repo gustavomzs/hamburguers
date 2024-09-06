ce3fee3eb98aedd18fc307a3ee7b30b035f18e3a
Gustavo <68656899+gustavomzs@users.noreply.github.com>
2024-09-06 17:03:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,72 @@
+let dados = [
+    {
+        // norte
+        titulo : "Amazonas",
+        descricao : "O famoso 'Hambúrguer de Pirarucu' é uma ótima opção para quem busca um sabor exótico. A carne do pirarucu, um peixe gigante da Amazônia, confere um sabor único e marcante ao hambúrguer",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "amazonas pirarucu peixe amazonia"
+    },
+    {
+        // Nordeste
+        titulo : "Bahia",
+        descricao :"O acarajé baiano é um clássico da culinária brasileira, mas você sabia que também existe uma versão em hambúrguer? O 'Acarajé Burger' é uma ótima opção para quem busca um sabor mais leve e refrescante.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : " Nordeste acarajé baiano bahia"
+    },
+    {
+        titulo : "Pernambuco",
+        descricao : "Recife é conhecida por suas diversas opções de hambúrgueres artesanais. O 'Hambúrguer de Costela' é uma opção popular, com carne desfiada e molho barbecue.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : "Nordeste recife costela barbecue pernambuco"
+    },
+    {
+        // Centro Oeste
+        titulo : "Mato Grosso do Sul",
+        descricao :"A carne de jacaré é uma iguaria regional, e alguns estabelecimentos oferecem o 'Hambúrguer de Jacaré' como uma opção exótica para os mais aventureiros.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Centro Oeste mato grosso jacaré iguaria"
+    },
+    {
+        // Sudeste
+        titulo : "Rio de Janeiro",
+        descricao :"O carioca adora um bom hambúrguer, e a cidade maravilhosa oferece diversas opções, desde os clássicos até os mais sofisticados. O 'Hambúrguer de Picanha' é uma opção popular, com carne macia e suculenta.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Sudeste rio carioca picanha"
+    },
+    {
+        titulo : "Minas Gerais",
+        descricao :"Em Minas Gerais, o 'Hambúrguer de Linguiça' é uma ótima opção para quem busca um sabor mais caseiro e tradicional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Sudeste minas gerias mineiro linguiça caseiro"
+    },
+    {
+        titulo : "São Paulo",
+        descricao :"A capital paulista é um verdadeiro paraíso para os amantes de hambúrguer, com diversas opções para todos os gostos. O 'Hambúrguer de Cordeiro' é um clássico, mas há também opções mais elaboradas, como o 'Hambúrguer de Costela' e o 'Hambúrguer de Pato'.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Sudeste sao paulo paulista cordeiro pato"
+    },
+    {
+        titulo : "Espírito Santo",
+        descricao :"O capixaba também aprecia um bom hambúrguer, e a cidade de Vitória oferece diversas opções, como o 'Hambúrguer de Tilápia', uma opção mais leve e saborosa.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Sudeste espirito santo capixaba vitória Tilápia tilapia peixe"
+    },
+    {
+        // Sul
+        titulo : "Rio Grande do Sul",
+        descricao :"O gaúcho é apaixonado por churrasco, e o 'Hambúrguer de Churrasco' é uma opção popular nos restaurantes gaúchos.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Sul rio grande do sul churrasco gaúchos gaucho"
+    },
+    {
+        titulo : "Santa Catarina",
+        descricao :"Santa Catarina é conhecida por suas belas praias e também por suas deliciosas opções de hambúrguer. O 'Hambúrguer de Camarão' é uma opção leve e saborosa para quem busca algo diferente.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Sul praias camarão "
+    },
+    {
+        titulo : "Paraná",
+        descricao :"O 'Hambúrguer de Carneiro' é uma opção exótica para quem busca um sabor mais intenso.",
+        tags : "Sul paraná carneiro"
+    },
+];
